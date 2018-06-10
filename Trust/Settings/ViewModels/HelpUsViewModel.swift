9f7c0fcf8be6ca143e6d1225a9db9bbb49a09b88
Steve Mulligan <steve@nonlocal.ca>
2018-06-09 23:35:21 -0400

    Change references from Ether to Ellaism
    Also changed references from trust to ellaism

--- Trust/Settings/ViewModels/HelpUsViewModel.swift
+++ Trust/Settings/ViewModels/HelpUsViewModel.swift
@@ -11,22 +11,22 @@ struct HelpUsViewModel {
     private let sharingText: [String] = [
         NSLocalizedString(
             "welldone.viewmodel.sharing.text1",
-            value: "Here is the app I use to store my ETH and ERC20 tokens.",
+            value: "Here is the app I use to store my Ellaism and ERC20 tokens.",
             comment: ""
         ),
         NSLocalizedString(
             "welldone.viewmodel.sharing.text2",
-            value: "Check out Trust - the wallet that lets me securely store my Ethereum and ERC20 tokens.",
+            value: "Check out Ellaism Wallet - the wallet that lets me securely store my Ellaism and ERC20 tokens.",
             comment: ""
         ),
         NSLocalizedString(
             "welldone.viewmodel.sharing.text3",
-            value: "I securely store Ethereum and ERC20 tokens in the Trust wallet",
+            value: "I securely store Ellaism and ERC20 tokens in the Ellaism Wallet",
             comment: ""
         ),
         NSLocalizedString(
             "welldone.viewmodel.sharing.text4",
-            value: "I secure my Ethereum and ERC20 tokens in the Trust wallet.",
+            value: "I secure my Ellaism and ERC20 tokens in the Ellaism Wallet.",
             comment: ""
         ),
     ]
